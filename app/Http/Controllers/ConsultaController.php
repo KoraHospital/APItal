209b7816869263918adc3d7cca59569c8d8bcce0
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-09-28 12:44:13 -0500

    CRUD Consultas

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Consulta;;
 use Validator;
-use App\Http\Resources\Consulta as CitaResource;
+use App\Http\Resources\Consulta as ConsultaResource;
 use App\Http\Requests;
 
 class ConsultaController extends Controller
@@ -18,18 +18,8 @@ class ConsultaController extends Controller
     public function index()
     {
         $consultas = Consulta::all();
-        $conversion = CitaResource::collection($consultas);
+        $conversion = ConsultaResource::collection($consultas);
         return response()->json($conversion);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -40,7 +30,30 @@ class ConsultaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $messages = [
+            'required' => 'El campo: :attribute, es requerido.',
+            'numeric' => 'El campo: :attribute, debe contener números.',
+        ];
+        $validator = Validator::make($request->all(), [
+            'cita_id' => 'required|numeric',
+            'personal_id' => 'required|numeric',
+            'medicamento_id' => 'required|numeric',
+        ], $messages);
+        
+        if ($validator->fails())
+        {
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+        }
+        else
+        {
+            $consultaActualizar = new Consulta;
+            $consultaActualizar->cita_id = $request->input('cita_id');
+            $consultaActualizar->personal_id = $request->input('personal_id');
+            $consultaActualizar->medicamento_id = $request->input('medicamento_id');
+            $consultaActualizar->save();
+            return response()->json($consultaActualizar);
+        }
     }
 
     /**
@@ -51,18 +64,9 @@ class ConsultaController extends Controller
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        $consultaMostrar = Consulta::findOrFail($id);
+        $conversion = new ConsultaResource($consultaMostrar);
+        return response()->json($conversion);
     }
 
     /**
@@ -74,7 +78,30 @@ class ConsultaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $messages = [
+            'required' => 'El campo: :attribute, es requerido.',
+            'numeric' => 'El campo: :attribute, debe contener números.',
+        ];
+        $validator = Validator::make($request->all(), [
+            'cita_id' => 'required|numeric',
+            'personal_id' => 'required|numeric',
+            'medicamento_id' => 'required|numeric',
+        ], $messages);
+        
+        if ($validator->fails())
+        {
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+        }
+        else
+        {
+            $consultaActualizar = Consulta::find($id);
+            $consultaActualizar->cita_id = $request->input('cita_id');
+            $consultaActualizar->personal_id = $request->input('personal_id');
+            $consultaActualizar->medicamento_id = $request->input('medicamento_id');
+            $consultaActualizar->save();
+            return response()->json($consultaActualizar);
+        }
     }
 
     /**
@@ -85,6 +112,10 @@ class ConsultaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $consulta = Consulta::findOrFail($id);
+        if($consulta->delete()){
+            $conversion = new ConsultaResource($consulta);
+            return response()->json($conversion);
+        }
     }
 }
